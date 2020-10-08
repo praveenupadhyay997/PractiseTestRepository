bbd89001b48081859b0714b4a998eb771cf1ba2a
praveenupadhyay997 <praveenupadhyay997@gmail.com>
2020-10-08 18:59:59 +0530

    [Praveen] Add: Added the Practise file

---
+++ TestClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestProjectPractise
+{
+    class TestClass
+    {
+
+        public int addNumber(int parameterOne, int parameterTwo)
+        {
+            return parameterOne + parameterTwo;
+        }
+
+        public int subtractNumber(int parameterOne, int parameterTwo)
+        {
+            return parameterOne - parameterTwo;
+        }
+
+    }
+}
---
+++ UnitTestClass.cs
@@ -0,0 +1,38 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestProjectPractise;
+
+namespace TestProjectPractise
+{
+    [TestClass]
+    public class UnitTestClass
+    {
+        [TestCategory("Addition")]
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //Arrange
+            TestClass testClass = new TestClass();
+            int expected = 25;
+            int numOne = 10;
+            int numTwo = 15;
+            //Act
+            var actual = testClass.addNumber(numOne, numTwo);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestCategory("Subtraction")]
+        [TestMethod]
+        public void TestMethod2()
+        {
+            //Arrange
+            TestClass testClass = new TestClass();
+            int expected = 5;
+            int numOne = 15;
+            int numTwo = 10;
+            //Act
+            var actual = testClass.subtractNumber(numOne, numTwo);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
